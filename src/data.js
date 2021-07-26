3b6abc19de2fc82bd8fc750d932c8f3d69742cd4
Min Park <minsungpark.msp@gmail.com>
2021-07-25 19:04:17 -0600

    added querySelectors

---
+++ src/data.js
@@ -0,0 +1,24 @@
+var sides = [
+  "Vegetables",
+  "French Fries",
+  "Salad",
+  "Spring Rolls",
+  "Cup of Soup",
+];
+
+var mains = [
+  "Pad Thai",
+  "Turf and Turf",
+  "Pho",
+  "Korean Bbq",
+  "Pizza",
+];
+
+var desserts = [
+  "Mango Lassi",
+  "Mochi",
+  "Boba",
+  "Strawberry Cheese Cake",
+  "Beer",
+];
+
